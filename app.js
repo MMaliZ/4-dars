bb7670efdafd9414b0e46824345e8daab56e50b7
muhammadAli <muhammadAli@gamil.com>
2025-02-07 13:59:42 +0500

    4-dars

---
+++ app.js
@@ -0,0 +1,38 @@
+
+
+// let wrap = document.querySelector(".wrap");
+
+// let h1 = document.createElement("h1");
+// h1.style.fontSize = "26px";
+// p.style.color = "red";
+// h1.textContent = "Assalomu alaykum";
+// let p = document.createElement("p");
+
+// h1.style.color = "orange";
+
+
+// p.style.fontSize = "20px";
+
+
+// p.textContent = "bu p";
+
+// wrap.appendChild(h1);
+// wrap.appendChild(p);
+
+// 2-mashq
+
+// let container = document.getElementById("container");
+
+// let firstP = container.firstElementChild;
+// let lastP = container.lastElementChild;
+
+// firstP.textContent = "Bu birinchi P";
+// lastP.textContent = "Bu oxirgi P";
+
+// 3-mashq
+
+// const listItems = document.querySelectorAll("li");
+
+// listItems.forEach((item) => {
+//   item.style.backgroundColor = "orange";
+// });
